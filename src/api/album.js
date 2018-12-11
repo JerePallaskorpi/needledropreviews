973e280f82f38a0d97d279917ff23e23821a7bbb
Jere Pallaskorpi <jere.pallaskorpi@sitowise.com>
2018-12-11 21:42:39 +0200

    Remove proxy

--- src/api/album.js
+++ src/api/album.js
@@ -1,3 +1,10 @@
-export const getAlbums = () => fetch('/api/albums')
+import { getHeaders } from './config';
+
+// Placeholder until invalid host with proxy fixed
+const url = window.location.href.includes('localhost')
+    ? 'http://localhost:8888/api/albums'
+    : 'https://tndreviews-api.herokuapp.com/api/albums';
+
+export const getAlbums = () => fetch(url, getHeaders())
     .then(res => res)
     .then(res => res.json());
