8114a68fb5bfb087b74c7e406b946b5f337f5930
Jere <jere.pallaskorpi@gmail.com>
2019-04-14 18:56:21 +0300

    Add detail update for db videos
    - Changed schedule to every 5 minutes for testing

--- server/queries/video.js
+++ server/queries/video.js
@@ -6,18 +6,34 @@ const db = require('../models');
  * @param {Object[]} playlistVideos List of videos found from playlist data.
  */
 const dbVideoCreate = async (playlistVideos) => {
-    let existingTitles = [];
+    let existingVideoIds = [];
 
     await db.Video.find({}, (err, videos) => {
-        if (videos.length) existingTitles = videos.map(a => a.title);
+        if (videos.length) existingVideoIds = videos.map(a => a.videoId);
     });
 
-    const videosToBeAdded = playlistVideos.filter(video => !existingTitles
-        .some(title => title === video.title));
+    const videosToBeAdded = playlistVideos.filter(video => !existingVideoIds
+        .some(videoId => videoId === video.videoId));
 
+    const videosToBeEdited = playlistVideos.filter(video => existingVideoIds
+        .some(videoId => videoId === video.videoId));
+
+    console.log(videosToBeAdded.length);
+    console.log(videosToBeEdited.length);
+
+    // Update video details for existing reviews.
+    videosToBeEdited.forEach((editedVideo) => {
+        db.Video.updateOne({ videoId: editedVideo.videoId },
+            { $set: { details: editedVideo.details } },
+            { upsert: true }, (err) => {
+                if (err) console.log(err);
+            });
+    });
+
+    // Insert new review videos.
     db.Video.insertMany(videosToBeAdded, { continueOnError: true }, err => (err
         ? console.log(err)
-        : console.log(`Database updated with ${videosToBeAdded.length} results.`)));
+        : console.log(`Database inserted with ${videosToBeAdded.length} results.`)));
 };
 
 module.exports = {
