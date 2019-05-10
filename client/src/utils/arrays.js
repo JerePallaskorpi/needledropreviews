18b68ebeadaa994af2f38a4d4394c50410b06e3d
Jere <jere.pallaskorpi@gmail.com>
2019-05-11 02:19:24 +0300

    Add util method for list sorting

--- client/src/utils/arrays.js
+++ client/src/utils/arrays.js
@@ -14,3 +14,22 @@ export const shuffleArray = arr => arr
 
 /** Sorts array by number values */
 export const sortNumber = (a, b) => a - b;
+
+/**
+ * Sorts album list with selected sorting method.
+ *
+ * @param {Object[]} reviewList List of reviews to be sorted.
+ * @param {string} sortBy Order to sort by.
+ *
+ * @returns {Object[]} Sorted review list.
+ */
+export const sortFilteredList = (reviewList: Object[], sortBy: string) => {
+    switch (sortBy) {
+        case 'newest':
+            return reviewList.sort((a, b) => new Date(b.date) - new Date(a.date));
+        case 'random':
+            return shuffleArray(reviewList);
+        default:
+            return reviewList;
+    }
+};
