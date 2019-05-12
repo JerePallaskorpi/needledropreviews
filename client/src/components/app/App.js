23039578f422564815278ea3db7f6580fae5599a
Jere <jere.pallaskorpi@gmail.com>
2019-05-12 16:32:03 +0300

    Remove redundant redux files

--- client/src/components/app/App.js
+++ client/src/components/app/App.js
@@ -1,14 +1,13 @@
 // @flow
 import React from 'react';
-import { Provider } from 'react-redux';
 import { GlobalStyle } from '../ui/defaultStyles';
 import AlbumReviewList from './album-review-list/AlbumReviewList';
 
-const App = ({ store }: any) => (
-    <Provider store={store}>
+const App = () => (
+    <>
         <GlobalStyle />
         <AlbumReviewList />
-    </Provider>
+    </>
 );
 
 export default App;
