ed1bbb6d8c7d860c81655d801172d47d2bf913eb
Jere <jere.pallaskorpi@gmail.com>
2019-05-12 16:32:03 +0300

    Remove redundant redux files

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/App';
 import registerServiceWorker from './registerServiceWorker';
-import store from './store';
 
-ReactDOM.render(<App store={store} />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
